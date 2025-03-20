110c806ee58758bf7a580a34b944a7fb94fe7d47
Tom Norton <sanic.go.fast.step.it.up@gmail.com>
2025-03-20 16:22:03 -0500

    day6 part 2 completed. ez dijkstra

--- day6.py
+++ day6.py
@@ -1,15 +1,19 @@
 import logging
 from sys import argv
-logging.basicConfig(level=logging.INFO)
+import heapq
+import time
+if argv[2] == 'debug':
+    logging.basicConfig(level=logging.DEBUG)
+else: 
+    logging.basicConfig(level=logging.INFO)
 
-# logging.info("Starting day6.py")
-# AAA)BBB, which means "BBB is in orbit around AAA".
+# run this with the following command:
+# python day6.py ./data/day6.txt info
 
 def get_data(loc):
     with open(loc) as f:
         rawdata = f.read().splitlines()
     return list(map(lambda x: x.split(')'),rawdata))
-orbit_list = get_data("./data/day6.txt")
 
 class CelestialBody:
     def __init__(self, name, parent=None, children=None):
@@ -17,7 +21,15 @@ class CelestialBody:
         self.parent = parent
         self.children = [] if children is None else children
         self.orbit_count = 0
+        self.dist=-2 #starting at -2 because the initial orbit jump and last orbit jump are not counted
+    def __lt__(self,other): # need this for the heapq comparison to work nicely
+        return self.dist < other.dist
+    def __eq__(self,other):
+        return self.dist == other.dist
 
+start = time.time()
+
+orbit_list = get_data(argv[1])
 celestial_bodies = {}   
 for orbit in orbit_list:
     logging.debug(orbit)
@@ -39,7 +51,7 @@ for orbit in orbit_list:
         child.parent = parent
     parent.children.append(child)
     
-mystack = [v for k,v in celestial_bodies.items() if v.parent==None]
+mystack = [v for v in celestial_bodies.values() if type(v.parent)==type(None)]
 
 part1=0
 iterations=0
@@ -48,8 +60,34 @@ while mystack:
     logging.debug(f"iterations: {iterations}, part1: {part1}, mystack: {len(mystack)}")
     logging.debug(f'body:{body.name}, children{[c.name for c in body.children]}')
     mystack.extend(body.children)
-    if body.parent != None:
+    if type(body.parent) != type(None):
         body.orbit_count = body.parent.orbit_count + 1
         part1+=body.orbit_count
     iterations+=1
 logging.info(f'part1: {part1}')
+
+# doing dijkstra's algorithm. setting up our heap and list of visited spots.
+mystack2 = [v for k,v in celestial_bodies.items() if k=='YOU']
+heapq.heapify(mystack2)
+visited =set()
+
+while mystack2:
+    current_body = heapq.heappop(mystack2) 
+    if current_body.name == 'SAN': #check to see if we've found our destination
+        logging.info(f'part2: {current_body.dist}')
+        break
+    if current_body.name in visited: #check to see if we've already visited this node
+        continue
+    visited.add(current_body.name) #if not, add it to our visited list
+    if type(current_body.parent) != type(None):
+        current_body.parent.dist = current_body.dist + 1
+        heapq.heappush(mystack2,current_body.parent)
+    for child in current_body.children:
+        child.dist = current_body.dist + 1
+        heapq.heappush(mystack2,child)
+
+logging.info(f'Time: {round(time.time()-start,5)} seconds.')
+
+# INFO:root:part1: 158090
+# INFO:root:part2: 241
+# Time: 0.02428 seconds.
