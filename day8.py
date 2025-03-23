4308d7b553f46406c570a0339710187a01b4c5ba
Tom Norton <sanic.go.fast.step.it.up@gmail.com>
2025-03-23 15:46:11 -0500

    day 8 p2. thought it wasn't working last night but it was.

--- day8.py
+++ day8.py
@@ -7,7 +7,7 @@ def get_data(loc):
     if len(image_enc)>20: 
         rows,cols=6,25
     else:
-        rows,cols=2,3
+        rows,cols=2,2
     layers = len(image_enc)/(rows*cols)
     # # Define the shape of the 3D array (e.g., 2 layers, 3 rows, 4 columns)
     shape = (layers,rows,cols)
@@ -27,5 +27,33 @@ def p1(array_3d):
 
     print('p1:',(array_3d[min_zero_layer]==1).sum()*(array_3d[min_zero_layer]==2).sum())
     
+# array_3d=get_data('./data/day8_ex2.txt')
 array_3d=get_data('./data/day8.txt')
 p1(array_3d)
+
+
+# initialize our picture with all missing pixels
+picture=np.zeros((array_3d.shape[1],array_3d.shape[2]))+2
+for layer in array_3d:
+    # print(picture)
+    # print(layer)
+    missing_mask = picture==2
+    if missing_mask.sum()==0: #no need to look further if we've populated all our pixels
+        print('finished early')
+        break
+    nonmissing_mask = layer!=2
+    new_pixels_to_fill = missing_mask * nonmissing_mask
+    picture[new_pixels_to_fill]=layer[new_pixels_to_fill]
+    # print('need to fill\n',missing_mask)
+    # print('can fill her\n',nonmissing_mask)
+    # print('fill it up  \n',new_pixels_to_fill)
+print('p2:')
+
+for i in picture.tolist():
+    print(''.join(['#' if j==1 else ' ' for j in i]))
+###  #  # #### ###  ###  
+#  # #  #    # #  # #  # 
+#  # #  #   #  ###  #  # 
+###  #  #  #   #  # ###  
+# #  #  # #    #  # #    
+#  #  ##  #### ###  #    
